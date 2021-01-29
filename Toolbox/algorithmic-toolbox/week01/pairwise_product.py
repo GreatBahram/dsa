068f88d77fdf16f7dd773ee553197febf54d2314
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-01-29 22:40:38 +0330

    add pairwise_product exercise

---
+++ Toolbox/algorithmic-toolbox/week01/pairwise_product.py
@@ -0,0 +1,30 @@
+import math
+from typing import List, Tuple
+
+
+def find_two_largest(nums: List[int]) -> Tuple[int]:
+    first = second = -math.inf
+    for number in nums:
+        if number >= first:
+            second = first
+            first = number
+        elif number >= second:
+            second = number
+    return (first, second)
+
+
+if __name__ == "__main__":
+    import operator
+    import sys
+    from functools import reduce
+
+    n = int(sys.stdin.readline())
+    nums = list(map(int, sys.stdin.readline().split()))
+
+    assert n == len(nums)
+
+    if n <= 2:
+        items = nums
+    else:
+        items = find_two_largest(nums)
+    print(reduce(operator.mul, items))
