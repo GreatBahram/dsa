ae22f8bf37cfb4a3553de3d503a047b67a54fe98
GreatBahram <aghaee.bahram@gmail.com>
2021-01-30 15:09:56 +0330

    add fib algorithm

---
+++ algorithmic-toolbox/week02/fib.py
@@ -0,0 +1,32 @@
+def rfib(n: int) -> int:
+    """Recursive Fibonacci."""
+    if n < 2:  # base case
+        return n
+    return rfib(n - 2) + rfib(n - 1)  # recursive case
+
+
+from typing import Dict
+
+memo: Dict[int, int] = {0: 0, 1: 1}
+
+
+def dfib(n):
+    """
+    Expedite the recursive Fibonacci algorithm using memoization technique.
+    d stands for using a dictionary as it memoization strucuture.
+    """
+    if n not in memo:
+        memo[n] = dfib(n - 1) + dfib(n - 2)
+    return memo[n]
+
+
+# Simplified dfib function using python's automatic memoization object.
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def mfib(n):
+    """Expedite the recursive Fibonacci algorithm using memoization technique."""
+    if n < 2:  # base case
+        return n
+    return mfib(n - 2) + mfib(n - 1)  # recursive case
