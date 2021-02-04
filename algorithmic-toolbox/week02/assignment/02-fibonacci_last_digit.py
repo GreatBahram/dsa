d4c72f1c8828f0aaa80355f282ab32ff6f1be078
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-02-04 14:40:08 +0330

    add some comments

--- algorithmic-toolbox/week02/assignment/02-fibonacci_last_digit.py
+++ algorithmic-toolbox/week02/assignment/02-fibonacci_last_digit.py
@@ -10,11 +10,12 @@ def fib_last_digit(n: int) -> int:
 
     a, b = 0, 1
 
-    for _ in range(n % PISANO_PERIOD):
+    for _ in range(n):
+        # We use % 10, becuase we're only interested in the last digit
         a, b = b, (a + b) % 10
     return a
 
 
 if __name__ == "__main__":
     n = int(input())
-    print(fib_last_digit(n))
+    print(fib_last_digit(n % PISANO_PERIOD))
