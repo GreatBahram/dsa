d5fa079a8e04d0dd1e806620f510ff2dd6564e16
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-02-04 12:03:46 +0330

    make last_digit more readable

--- algorithmic-toolbox/week02/assignment/fibonacci_last_digit.py
+++ algorithmic-toolbox/week02/assignment/fibonacci_last_digit.py
@@ -1,3 +1,6 @@
+PISANO_PERIOD: int = 60
+
+
 def fib_last_digit(n: int) -> int:
     """
     Fibonacci numbers repeats with a cycle length of 60.
@@ -7,10 +10,11 @@ def fib_last_digit(n: int) -> int:
 
     a, b = 0, 1
 
-    for _ in range(n % 60):
+    for _ in range(n % PISANO_PERIOD):
         a, b = b, (a + b) % 10
     return a
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     n = int(input())
     print(fib_last_digit(n))
