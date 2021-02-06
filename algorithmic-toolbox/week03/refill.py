dd6645131485f36f719bbd5b7e28fc79cbd95070
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-02-06 18:16:41 +0330

    add refill exercise

---
+++ algorithmic-toolbox/week03/refill.py
@@ -0,0 +1,31 @@
+from numbers import Number
+from typing import List
+
+
+def min_refill(stations: List[Number], limit: int):
+    """
+    Don't forget to add start and end point into stations variable.
+    If you need further investigation, check this one:
+    https://stackoverflow.com/q/61570575/7093905
+    """
+    num_refills = current_refill = 0
+
+    while current_refill < len(stations) - 1:
+        last_refill = current_refill
+
+        # find the farthest point, if it is possible:
+        # first condition: checks to make sure we are reached to the destination
+        # second condition: is this the farthest stations?
+        while (current_refill < len(stations) - 1) and (
+            stations[current_refill + 1] - stations[last_refill] <= limit
+        ):
+            current_refill += 1
+
+        # we couldn't find neXt step
+        if current_refill == last_refill:
+            raise ValueError("Impossible to reach the destination.")
+
+        if current_refill < len(stations) - 1:
+            num_refills += 1
+
+    return num_refills
