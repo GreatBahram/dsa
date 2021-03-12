c1b4c1028d99d8e4fc6f92ed49f1ea4abfc66706
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-03-12 14:33:00 +0330

    add both edit_distance and lcs2 exercise

---
+++ algorithmic-toolbox/week05/assignment/edit_distance.py
@@ -0,0 +1,55 @@
+import math
+
+
+def top_down_edit_distance(str1: str, str2: str, m: int, n: int) -> int:
+    """
+    m: is len(str1)
+    n: is len(str2)
+    """
+    if m == 0:
+        return n
+    if n == 0:
+        return m
+
+    if str1[m - 1] == str2[n - 1]:
+        return top_down_edit_distance(str1, str2, m - 1, n - 1)
+    return 1 + min(
+        top_down_edit_distance(str1, str2, m, n - 1),
+        top_down_edit_distance(str1, str2, m - 1, n),
+        top_down_edit_distance(str1, str2, m - 1, n - 1),
+    )
+
+
+def generate_memo(rows: int, cols: int):
+    memo = []
+    row = [0] + [math.inf for i in range(cols)]
+
+    memo = [list(row) for i in range(rows + 1)]
+
+    memo[0] = [i for i in range(cols + 1)]
+    for i in range(rows + 1):
+        memo[i][0] = i
+
+    return memo
+
+
+def edit_distance(str1: str, str2: str) -> int:
+    """
+    Return the minimum number of operations (insertions, deletions, and
+    substitutions of symbols) to transform one string into another
+    """
+    memo = generate_memo(len(str2), len(str1))
+    str1 = "_" + str1
+    str2 = "_" + str2
+    for j in range(1, len(str2)):
+        for i in range(1, len(str1)):
+            if str1[i] == str2[j]:
+                memo[j][i] = memo[j - 1][i - 1]
+            else:
+                memo[j][i] = min(memo[j][i - 1], memo[j - 1][i], memo[j - 1][i - 1]) + 1
+    min_ops = memo[-1][-1]
+    return min_ops
+
+
+if __name__ == "__main__":
+    print(edit_distance(input(), input()))
---
+++ algorithmic-toolbox/week05/assignment/lcs2.py
@@ -0,0 +1,48 @@
+"""Longest common subsequence of two sequences.
+If you want to understand the way I solved this problem,
+watch this video on Youtube:
+https://www.youtube.com/watch?v=NnD96abizww
+"""
+
+import math
+import sys
+
+
+def generate_memo(cols: int, rows: int):
+    memo = []
+    row = [0] + [math.inf for _ in range(cols)]
+    memo = [list(row) for i in range(rows + 1)]
+    # specific problem changes
+    # Reason: because there is zero subsequence between null string and other strings.
+    memo[0] = [0 for item in memo[0]]
+    return memo
+
+
+def lcs2(a: str, b: str):
+    memo = generate_memo(len(a), len(b))
+    a = "_" + a
+    b = "_" + b
+
+    for j in range(1, len(b)):
+        for i in range(1, len(a)):
+            if a[i] == b[j]:
+                memo[j][i] = 1 + memo[j - 1][i - 1]
+            else:
+                memo[j][i] = max(memo[j - 1][i], memo[j][i - 1])
+    return memo
+
+
+if __name__ == "__main__":
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+
+    n = data[0]
+    data = data[1:]
+    a = data[:n]
+
+    data = data[n:]
+    m = data[0]
+    data = data[1:]
+    b = data[:m]
+
+    print(lcs2("".join(a), "".join(b)))
