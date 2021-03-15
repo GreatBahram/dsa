03415a39c5692e9e7e6e194c80a43d6a8c5277ef
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-03-15 17:17:04 +0330

    Update edit_distance.py
    Add memoization to the top-down one.

--- algorithmic-toolbox/week05/assignment/edit_distance.py
+++ algorithmic-toolbox/week05/assignment/edit_distance.py
@@ -1,13 +1,13 @@
 import math
 
 
-def top_down_edit_distance(str1: str, str2: str, m: int, n: int) -> int:
+def recursive_edit_distance(str1: str, str2: str, m: int, n: int) -> int:
     """
     m: is len(str1)
     n: is len(str2)
     It will take a long time for a large strings.
     example:
-    >>> top_down_edit_distance("dinitrophenylhydrazine", "benzalphenylhydrazone", 22, 21)
+    >>> recusrive_edit_distance("dinitrophenylhydrazine", "benzalphenylhydrazone", 22, 21)
 
     """
     if m == 0:
@@ -16,12 +16,57 @@ def top_down_edit_distance(str1: str, str2: str, m: int, n: int) -> int:
         return m
 
     if str1[m - 1] == str2[n - 1]:
-        return top_down_edit_distance(str1, str2, m - 1, n - 1)
+        return recursive_edit_distance(str1, str2, m - 1, n - 1)
     return 1 + min(
-        top_down_edit_distance(str1, str2, m, n - 1),
-        top_down_edit_distance(str1, str2, m - 1, n),
-        top_down_edit_distance(str1, str2, m - 1, n - 1),
+        recursive_edit_distance(str1, str2, m, n - 1),
+        recursive_edit_distance(str1, str2, m - 1, n),
+        recursive_edit_distance(str1, str2, m - 1, n - 1),
     )
+
+
+def top_down_edit_distance(str1, str2):
+    """
+    m: is len(str1)
+    n: is len(str2)
+    """
+    row = [-1 for i in range(len(str1) + 1)]
+    memo = [list(row) for i in range(len(str2) + 1)]
+
+    def tp_edit_distance(str1, str2, m: int, n: int):
+        if m == 0:
+            return n
+        if n == 0:
+            return m
+
+        if memo[n][m] != -1:
+            return memo[n][m]
+
+        if str1[m - 1] == str2[n - 1]:
+            if memo[n - 1][m - 1] != -1:
+                memo[n][m] = memo[n - 1][m - 1]
+                return memo[n][m]
+            else:
+                memo[n][m] = tp_edit_distance(str1, str2, m - 1, n - 1)
+                return memo[n][m]
+        else:
+            if memo[n - 1][m] != -1:
+                value1 = memo[n - 1][m]
+            else:
+                value1 = tp_edit_distance(str1, str2, m -1, n)
+
+            if memo[n - 1][m - 1] != -1:
+                value2 = memo[n - 1][m - 1]
+            else:
+                value2 = tp_edit_distance(str1, str2, m -1, n - 1)
+
+            if memo[n][m - 1] != -1:
+                value3 = memo[n][m - 1]
+            else:
+                value3 = tp_edit_distance(str1, str2, m, n - 1)
+            memo[n][m] = min(value1, value2, value3) + 1
+            return memo[n][m]
+
+    return tp_edit_distance(str1, str2, len(str1), len(str2))
 
 
 def generate_memo(rows: int, cols: int):
