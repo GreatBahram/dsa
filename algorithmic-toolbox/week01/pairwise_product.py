364ac42552b46e7eaf08fb7cfbced93e5441d00f
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-03-24 10:27:12 +0430

    remove unnecessary imports and add some document

--- algorithmic-toolbox/week01/pairwise_product.py
+++ algorithmic-toolbox/week01/pairwise_product.py
@@ -1,8 +1,37 @@
+"""
+Find the maximum product of two distinct num-
+bers in a sequence of non-negative integers.
+"""
 import math
 from typing import List, Tuple
 
 
-def find_two_largest(nums: List[int]) -> Tuple[int]:
+def naive_algorithm(nums: List[int]) -> int:
+    """
+    Time complexity: O(n**2)
+    """
+    max_product = -math.inf
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if max_product < nums[i] * nums[j]:
+                max_product = nums[i] * nums[j]
+    return max_product
+
+
+def second_approach(nums: List[int]) -> int:
+    """
+    Sorted numbers in O(nlogn) and pick two largest items
+    """
+    max2, max1 = sorted(nums)[-2:]
+    return max2 * max1
+
+
+def best_approach(nums: List[int]) -> Tuple[int]:
+    """
+    Time complexity: O(n)
+    Space complexity: O(1)
+    """
     if len(nums) <= 2:
         return nums
 
@@ -14,17 +43,13 @@ def find_two_largest(nums: List[int]) -> Tuple[int]:
             first = number
         elif number >= second:
             second = number
-    return (first, second)
+    return first * second
 
 
 if __name__ == "__main__":
-    import operator
-    import sys
-    from functools import reduce
-
-    n = int(sys.stdin.readline())
-    nums = list(map(int, sys.stdin.readline().split()))
+    n = int(input())
+    nums = list(map(int, input().split()))
 
     assert n == len(nums)
 
-    print(reduce(operator.mul, find_two_largest(nums)))
+    print(best_approach(nums))
