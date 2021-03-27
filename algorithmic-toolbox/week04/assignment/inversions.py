27843ee57c362c86bf72488ff82465a69bed6510
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-03-27 16:28:12 +0430

    Rewrite inversions to be more readable

--- algorithmic-toolbox/week04/assignment/inversions.py
+++ algorithmic-toolbox/week04/assignment/inversions.py
@@ -1,66 +1,65 @@
 import sys
+from typing import List
 
 
-def get_number_of_inversions(numbers, tmp, left, right) -> int:
+def brute_force(array):
+    """
+    Time complexity: O(n**2)
+    Space complexity: O(1)
+    """
     inversions = 0
+    n = len(array)
 
-    if left < right:
-        mid = (left + right) // 2
-
-        inversions += get_number_of_inversions(numbers, tmp, left, mid)
-        inversions += get_number_of_inversions(numbers, tmp, mid + 1, right)
-
-        inversions += merge(numbers, tmp, left, mid, right)
-
-    return inversions
-
-
-def merge(numbers, tmp, left, mid, right):
-    i = k = left
-    j = mid + 1
-    inversions = 0
-
-    while i <= mid and j <= right:
-        if numbers[i] <= numbers[j]:
-            tmp[k] = numbers[i]
-            i += 1
-            k += 1
-        else:
-            # inversion will occur here
-            tmp[k] = numbers[j]
-            j += 1
-            k += 1
-            inversions += mid - i + 1
-
-    while i <= mid:
-        tmp[k] = numbers[i]
-        i += 1
-        k += 1
-
-    while j <= right:
-        tmp[k] = numbers[j]
-        j += 1
-        k += 1
-
-    # copy sorted array from the temp array into the original one
-    for idx in range(left, right + 1):
-        numbers[idx] = tmp[idx]
-
-    return inversions
-
-
-def brute_force(array, n):
-    inversions = 0
-    for i in range():
-        for j in range(i + 1, n)):
+    for i in range(n):
+        for j in range(i + 1, n):
             if array[j] > array[i]:
                 inversions += 1
     return inversions
 
 
+def merge_sort_with_inversion(array: List[int]):
+    if len(array) < 2:
+        return array, 0
+
+    mid = len(array) // 2
+    left = array[:mid]
+    right = array[mid:]
+
+    left, left_inversions = merge_sort_with_inversion(left)
+    right, right_inversions = merge_sort_with_inversion(right)
+
+    inversions = left_inversions + right_inversions
+
+    sorted_array, merge_inverions = merge_with_inversion(left, right)
+    return sorted_array, inversions + merge_inverions
+
+
+def merge_with_inversion(left, right):
+    left_idx = right_idx = inversions = 0
+    mid = len(right) - 1
+    results = []
+
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] <= right[right_idx]:
+            results.append(left[left_idx])
+            left_idx += 1
+        else:
+            results.append(right[right_idx])
+            right_idx += 1
+            # [4, 5], [3, 2, 1]
+            inversions += len(left) - left_idx
+
+    if left:
+        results.extend(left[left_idx:])
+
+    if right:
+        results.extend(right[right_idx:])
+
+    return results, inversions
+
+
 if __name__ == "__main__":
     input = sys.stdin.read()
     n, *numbers = list(map(int, input.split()))
-    # we use this temporary array to save temporarily save the sorted array inside it.
-    tmp = n * [0]
-    print(get_number_of_inversions(numbers, tmp, 0, n - 1))
+    sorted_array, inversions = merge_sort_with_inversion(numbers)
+    print(inversions)
