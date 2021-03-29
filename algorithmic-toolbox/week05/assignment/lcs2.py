0122103cac9a4c1f5d6a91d405f52421f170b35f
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-03-29 16:37:49 +0430

    add the naive and top down version for lcs2

--- algorithmic-toolbox/week05/assignment/lcs2.py
+++ algorithmic-toolbox/week05/assignment/lcs2.py
@@ -8,6 +8,60 @@ import math
 import sys
 
 
+def naive_recursive_lcs2(str1: str, str2: str, m: int, n: int) -> int:
+    """
+    m: is len(str1)
+    n: is len(str2)
+    """
+    if m == 0:
+        return 0
+    if n == 0:
+        return 0
+
+    if str1[m - 1] == str2[n - 1]:
+        return naive_recursive_lcs2(str1, str2, m - 1, n - 1) + 1
+    return max(
+        naive_recursive_lcs2(str1, str2, m - 1, n),
+        naive_recursive_lcs2(str1, str2, m, n - 1),
+    )
+
+
+def tp_lcs2(str1: str, str2: str) -> int:
+    """
+    Top-down longest common subsequence
+    """
+    row = [-1 for _ in range(len(str1) + 1)]
+    memo = [list(row) for _ in range(len(str2) + 1)]
+
+    def recursive_lcs2(str1: str, str2: str, m: int, n: int) -> int:
+        if m == 0 or n == 0:
+            return 0
+
+        if memo[n][m] != -1:
+            return memo[n][m]
+
+        if str1[m - 1] == str2[n - 1]:
+            if memo[n - 1][m - 1] != -1:
+                memo[n][m] = memo[n - 1][m - 1] + 1
+            else:
+                memo[n][m] = recursive_lcs2(str1, str2, m - 1, n - 1) + 1
+            return memo[n][m]
+        else:
+            if memo[n - 1][m] != -1:
+                value1 = memo[n - 1][m]
+            else:
+                value1 = recursive_lcs2(str1, str2, m, n - 1)
+
+            if memo[n][m - 1] != -1:
+                value2 = memo[n][m - 1]
+            else:
+                value2 = recursive_lcs2(str1, str2, m - 1, n)
+
+            return max(value1, value2)
+
+    return recursive_lcs2(str1, str2, len(str1), len(str2))
+
+
 def generate_memo(cols: int, rows: int):
     memo = []
     row = [0] + [math.inf for _ in range(cols)]
@@ -18,8 +72,11 @@ def generate_memo(cols: int, rows: int):
     return memo
 
 
-def lcs2(a: str, b: str):
+def lcs2(a: List[str], b: List[str]):
+    """Bottom-up longest common subsequence of two sequences."""
     memo = generate_memo(len(a), len(b))
+    # NOTE: I added the None prefix to make the calculation easy,
+    # for instance a[i] == b[j].
     a = [None] + a
     b = [None] + b
 
