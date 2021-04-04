32b052fe741deb41d5c2c8e8c20e4522cb2d7c2c
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-04-04 20:55:07 +0430

    add solution for tree_height

--- data-structures/week01/assignments/tree_height.py
+++ data-structures/week01/assignments/tree_height.py
@@ -3,8 +3,22 @@ import threading
 
 
 class Node:
-    def __init__(self):
+    def __init__(self, idx=None):
+        self.idx = idx
         self.parent = None
+
+    def __repr__(self):
+        return f"{type(self).__name__}(index={self.idx}, parent={self.parent})"
+
+
+class Node2:
+    """Each node knows about its children, not its parent."""
+    def __init__(self, idx=None):
+        self.idx = idx
+        self.children = set()
+
+    def __repr__(self):
+        return f"{type(self).__name__}(index={self.idx}, children={self.children!r})"
 
 
 def compute_height(n, parents):
@@ -21,7 +35,7 @@ def compute_height(n, parents):
 
 
 def find_max_height(node: Node):
-    count = 0
+    count = 1
     while node.parent is not None:
         node = node.parent
         count += 1
@@ -35,21 +49,60 @@ def max_height(nodes):
     return max(heights)
 
 
-def main():
-    n = int(input())
-    parents = list(map(int, input().split()))
-    print(compute_height(n, parents))
+def first_approach(n, parents):
+    """
+    Recursive approach which calculate height for each node
+    and then return the maximum one.
+    """
+    # initialize list of nodes
+    nodes = [Node(i) for i in range(n)]
 
-    for idx, parent_id in enumerate(items):
+    # connect each node to its parent
+    for idx, parent_id in enumerate(parents):
         if parent_id == -1:
-            nodes[idx].parent = ROOT_NODE
+            nodes[idx].parent = None
         else:
             nodes[idx].parent = nodes[parent_id]
 
+    print(max_height(nodes))
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10 ** 7)  # max depth of recursion
-threading.stack_size(2 ** 27)  # new thread will get stack of such size
-threading.Thread(target=main).start()
+
+def second_approach(n, parents):
+    """
+    In this implementation I went for calculating depth
+    from the head node, recursively.
+    """
+    # initialize list of nodes
+    nodes = [Node2(i) for i in range(n)]
+
+    # connect each node to its parent
+    for idx, parent_id in enumerate(parents):
+        if parent_id == -1:
+            head = nodes[idx]
+        else:
+            nodes[parent_id].children.add(nodes[idx])
+
+    print(compute_depth(head))
+
+
+def compute_depth(node: Node2):
+    if not node.children:
+        return 1
+    return 1 + max(compute_depth(ch) for ch in node.children)
+
+
+def main():
+    n = int(input())
+    parents = list(map(int, input().split()))
+    # print(compute_height(n, parents))
+    # first_approach(n, parents)
+    second_approach(n, parents)
+
+
+if __name__ == "__main__":
+    # In Python, the default limit on recursion depth is rather low,
+    # so raise it here for this problem. Note that to take advantage
+    # of bigger stack, we have to launch the computation in a new thread.
+    sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+    threading.stack_size(2 ** 27)  # new thread will get stack of such size
+    threading.Thread(target=main).start()
