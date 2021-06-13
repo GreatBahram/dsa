2d1b67a2ced31e6c3a37d6b1dfeb0a5bc23368c1
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-06-14 01:57:30 +0430

    WIP: runaway problem using bst

---
+++ data-structures/week05/runaway.py
@@ -0,0 +1,21 @@
+"""
+http://www.algorithmsandme.com/runway-reservation-system/
+
+MIT Binary Search Trees
+https://www.youtube.com/watch?v=9Jry5-82I68
+"""
+
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = self.right = None
+
+
+def find_position(node, key):
+    pass
+
+
+def insert(node, key):
+    find_position(node, key)
+    pass
