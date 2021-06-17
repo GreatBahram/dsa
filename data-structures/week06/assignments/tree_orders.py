2cd900f40b391f194dc8bcc8cbeb40497b19a5bd
GreatBahram <aghaee.bahram@gmail.com>
2021-06-17 19:11:35 +0430

    WIP: tree traversals

---
+++ data-structures/week06/assignments/tree_orders.py
@@ -0,0 +1,90 @@
+import sys
+import threading
+from typing import List, NamedTuple
+
+# sys.setrecursionlimit(10 ** 6)  # max depth of recursion
+# threading.stack_size(2 ** 27)  # new thread will get stack of such size
+
+
+class Node:
+    def __init__(self, value: int) -> None:
+        self.value = value
+        self.left = self.right = None
+
+
+class VertexInfo(NamedTuple):
+    value: int
+    left_idx: int
+    right_idx: int
+
+
+def read_inputs():
+    n = int(input())
+
+    array = [None] * n
+    for i in range(n):
+        array[i] = VertexInfo(*map(int, input().split()))
+    return array
+
+
+def create_tree(list_of_vertices: List[VertexInfo]) -> Node:
+    nodes = [Node(value) for value, *_ in list_of_vertices]
+    # shape the tree structure
+    for idx, (_, left_idx, right_idx) in enumerate(list_of_vertices):
+        node = nodes[idx]
+        node.left = nodes[left_idx] if left_idx >= 0 else None
+        node.right = nodes[right_idx] if right_idx >= 0 else None
+    return nodes[0]
+
+
+def inorder(node: Node):
+    if not node:
+        return None
+    inorder(node.left)
+    print(node.value)
+    inorder(node.right)
+
+
+def preorder(node: Node):
+    if not node:
+        return None
+    print(node.value)
+    preorder(node.left)
+    preorder(node.right)
+
+
+def postorder(node: Node):
+    if not node:
+        return None
+
+    postorder(node.left)
+    postorder(node.right)
+    print(node.value)
+
+
+# def main():
+
+
+
+if __name__ == "__main__":
+    # threading.Thread(target=main).start()
+    from contextlib import redirect_stdout
+    from io import StringIO
+    
+    list_of_vertices_info = read_inputs()
+    root = create_tree(list_of_vertices_info)
+    
+    fp = StringIO()
+    with redirect_stdout(fp):
+        inorder(root)
+    print(fp.getvalue().strip().replace('\n', ' '))
+
+    fp = StringIO()
+    with redirect_stdout(fp):
+        preorder(root)
+    print(fp.getvalue().strip().replace('\n', ' '))
+
+    fp = StringIO()
+    with redirect_stdout(fp):
+        postorder(root)
+    print(fp.getvalue().strip().replace('\n', ' '))
