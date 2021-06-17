9d8380a7f66600b3bea7597ee61385d2805c1ee6
GreatBahram <aghaee.bahram@gmail.com>
2021-06-17 17:06:00 +0430

    solve runaway reservation problem using a simple bst

--- data-structures/week05/runaway.py
+++ data-structures/week05/runaway.py
@@ -4,18 +4,50 @@ http://www.algorithmsandme.com/runway-reservation-system/
 MIT Binary Search Trees
 https://www.youtube.com/watch?v=9Jry5-82I68
 """
+from dataclasses import dataclass
 
 
+@dataclass
 class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = self.right = None
+    value: int
+    left: "Node" = None
+    right: "Node" = None
 
 
-def find_position(node, key):
-    pass
+def inorder(node: Node):
+    if not node:
+        return None
+    inorder(node.left)
+    print(node.value)
+    inorder(node.right)
 
 
-def insert(node, key):
-    find_position(node, key)
-    pass
+def add_node(node: Node, value: int, k: int):
+    if not node:
+        return Node(value)
+
+    if (node.value - k < value) and (node.value + k > value):
+        return node
+
+    if node.value > value:
+        node.left = add_node(node.left, value, k)
+    else:
+        node.right = add_node(node.right, value, k)
+
+    return node
+
+
+def main():
+    root = add_node(None, 30, 3)
+    add_node(root, 20, 3)
+    add_node(root, 15, 3)
+    add_node(root, 25, 3)
+    add_node(root, 40, 3)
+    add_node(root, 38, 3)
+    add_node(root, 45, 3)
+
+    inorder(root)
+
+
+if __name__ == "__main__":
+    main()
