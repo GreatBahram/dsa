e1ca42f76a12daa668261a5f0c529d5adbe082c1
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-07-29 20:59:01 +0430

    Solve both reachability and connected_components exercise reachability finds out whether there is a path between given vertices connected_components: calculate the number of connected components using DFS ordering

--- algorithms-on-graph/week01/assignments/connected_components.py
+++ algorithms-on-graph/week01/assignments/connected_components.py
@@ -1,21 +1,51 @@
-#Uses python3
+from collections import defaultdict
+from typing import List
 
-import sys
+
+class Graph:
+    def __init__(self, number_of_vertices: int) -> None:
+        self.adj = defaultdict(list)
+
+        for v in range(1, number_of_vertices + 1):
+            self.adj[v]
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({repr(dict(self.adj))})"
+
+    def add(self, vertex1, vertex2) -> None:
+        self.adj[vertex1].append(vertex2)
+        self.adj[vertex2].append(vertex1)
+
+    def update(self, edges) -> None:
+        """Update the graph by a given list of edges."""
+        for v1, v2 in edges:
+            self.add(v1, v2)
+
+    def get_connected_components(self) -> List[List[int]]:
+        visited = set()
+        cc = []
+
+        for vertex in self.adj:
+            if vertex not in visited:
+                temp = []
+                cc.append(self.explore(vertex, visited, temp))
+        return cc
+
+    def explore(self, vertex, visited, temp) -> List[List[int]]:
+        visited.add(vertex)
+        temp.append(vertex)
+
+        for neighbor in self.adj[vertex]:
+            if neighbor not in visited:
+                temp = self.explore(neighbor, visited, temp)
+        return temp
 
 
-def number_of_components(adj):
-    result = 0
-    #write your code here
-    return result
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    edges = [tuple(map(int, input().split())) for _ in range(m)]
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(number_of_components(adj))
+    graph = Graph(n)
+    graph.update(edges)
+
+    print(graph.get_connected_components())
--- algorithms-on-graph/week01/assignments/reachability.py
+++ algorithms-on-graph/week01/assignments/reachability.py
@@ -1,21 +1,51 @@
-#Uses python3
+from collections import defaultdict
 
-import sys
 
-def reach(adj, x, y):
-    #write your code here
-    return 0
+class Graph:
+    def __init__(self, number_of_vertices: int) -> None:
+        self.adj = defaultdict(list)
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    x, y = data[2 * m:]
-    adj = [[] for _ in range(n)]
-    x, y = x - 1, y - 1
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+        for v in range(1, number_of_vertices + 1):
+            self.adj[v]
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({repr(dict(self.adj))})"
+
+    def add(self, vertex1, vertex2) -> None:
+        self.adj[vertex1].append(vertex2)
+        self.adj[vertex2].append(vertex1)
+
+    def update(self, edges) -> None:
+        """Update the graph by a given list of edges."""
+        for v1, v2 in edges:
+            self.add(v1, v2)
+
+    def is_connected(self, v1, v2) -> None:
+        """
+        Return True if there is a connection; False otherwise.
+        """
+        # if it is connected directly, then don't search for it.
+        if v2 in self.adj[v1]:
+            return True
+
+        visited = set()
+        self.explore(v1, visited)
+        return v2 in visited
+
+    def explore(self, vertex1, visited: set) -> None:
+        visited.add(vertex1)
+        for neighbor in self.adj[vertex1]:
+            if neighbor not in visited:
+                self.explore(neighbor, visited)
+        return None
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    edges = [tuple(map(int, input().split())) for _ in range(m)]
+    query_vertices = list(map(int, input().split()))
+
+    graph = Graph(n)
+    graph.update(edges)
+
+    print(int(graph.is_connected(*query_vertices)))
