219880cfb638e0d10b2352add5f5c06e262fb869
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-07-29 15:12:09 +0430

    Add a solution to detect a cycle inside an undirected graph

--- algorithms-on-graph/week01/graphs.py
+++ algorithms-on-graph/week01/graphs.py
@@ -4,8 +4,28 @@ from typing import TypeVar
 Vertex = TypeVar("Vertex")
 
 
+class AdjacencyList:
+    def __init__(self):
+        self.adj = defaultdict(list)
+
+    def __repr__(self):
+        return repr(dict(self.adj))
+
+    def add(self, v: Vertex, w: Vertex) -> None:
+        """Add an edge from v to w and vice versa."""
+        self.adj[v].append(w)
+        self.adj[w].append(v)
+
+    @classmethod
+    def from_edges(cls, list_of_edges: list[tuple[Vertex, Vertex]]) -> "AdjacencyList":
+        graph = cls()
+        for v1, v2 in list_of_edges:
+            graph.add(v1, v2)
+        return graph
+
+
 # calculate the number of connected components
-class Graph:
+class Graph(AdjacencyList):
     """
     >>> g = Graph()
     >>> g.add('a', 'b')
@@ -14,56 +34,113 @@ class Graph:
     >>> len(g.get_connected_components())
     2
     """
-    def __init__(self):
-        self.adj = defaultdict(list)
-
-    def __repr__(self):
-        return repr(dict(self.adj))
-
-    def add(self, v: Vertex, w: Vertex) -> None:
-        """Add an edge from v to w and vice versa."""
-        self.adj[v].append(w)
-        self.adj[w].append(v)
 
     def get_connected_components(self) -> list[list[Vertex]]:
-        visited = dict.fromkeys(self.adj, False)
+        visited = set()
         cc = []
         for vertex in self.adj:
-            if not visited[vertex]:
+            if vertex not in visited:
                 temp = []
                 cc.append(self.explore(vertex, visited, temp))
         return cc
 
     def explore(self, vertex, visited: dict, temp: list):
-        visited[vertex] = True
+        visited.add(vertex)
         temp.append(vertex)
         for neighbor in self.adj[vertex]:
-            if not visited[neighbor]:
+            if neighbor not in visited:
                 temp = self.explore(neighbor, visited, temp)
         return temp
 
 
 # Checking whether there is a path between the given two vertices
-class Graph2:
-    def __init__(self):
-        self.adj = defaultdict(list)
-
-    def __repr__(self):
-        return repr(dict(self.adj))
-
-    def add(self, v: Vertex, w: Vertex) -> None:
-        """Add an edge from v to w and vice versa."""
-        self.adj[v].append(w)
-        self.adj[w].append(v)
+class Graph2(AdjacencyList):
+    """
+    >>> g = Graph2()
+    >>> g.add('a', 'b')
+    >>> g.add('b', 'c')
+    >>> g.add('d', 'e')
+    >>> g.is_connected('a', 'c')
+    True
+    >>> g.is_connected('b', 'e')
+    False
+    """
 
     def explore(self, vertex, visited: dict):
-        """DFS implmentation"""
-        visited[vertex] = True
+        """DFS implementation"""
+        visited.add(vertex)
         for neighbor in self.adj[vertex]:
-            if not visited[neighbor]:
+            if neighbor not in visited:
                 self.explore(neighbor, visited)
 
-    def find_path(self, vertex1, vertex2) -> bool:
-        visited = dict.fromkeys(self.adj, False)
+    def is_connected(self, vertex1, vertex2) -> bool:
+        visited = set()
         self.explore(vertex1, visited)
-        return vertex2 in (v for v, is_visited in visited.items() if is_visited)
+        return vertex2 in visited
+
+
+# checking whether there is a cycle
+class Graph3(AdjacencyList):
+    """
+    You might find the explanation of this video beneficial
+    >>> g = Graph3()
+    >>> g.add('a', 'b')
+    >>> g.add('b', 'c')
+    >>> g.add('c', 'd')
+    >>> g.add('b', 'd')
+    >>> g.is_cyclic()
+    True
+    """
+
+    def explore(self, vertex, visited: set, parent):
+        """
+        Using DFS algorithm look for back-edge
+        A Back Edge is an edge that connects a vertex to a vertex that is
+        discovered before it's parent.
+        """
+        visited.add(vertex)
+        for neighbor in self.adj[vertex]:
+            if neighbor not in visited:
+                if self.explore(neighbor, visited, vertex):
+                    return True
+            else:
+                # if it has been visited, check whether it is our
+                # parent or not, if it isn't, then it is a back edge.
+                if neighbor != parent:
+                    return True
+        return False
+
+    def is_cyclic(self) -> bool:
+        visited = set()
+
+        for vertex in self.adj:
+            if vertex not in visited:
+                if self.explore(vertex, visited, -1):
+                    return True
+        return False
+
+    def get_cycle_path(self) -> str:
+        if not self.is_cyclic():
+            return ""
+
+        temp = []
+        visited = set()
+
+        for vertex in self.adj:
+            if vertex not in visited:
+                self._explore(vertex, visited, -1, temp)
+        return " -> ".join(map(str, temp))
+
+    def _explore(self, vertex, visited, parent, temp) -> bool:
+        visited.add(vertex)
+        temp.append(vertex)
+
+        for neighbor in self.adj[vertex]:
+            if neighbor not in visited:
+                if self._explore(neighbor, visited, vertex, temp):
+                    return True
+            else:
+                if neighbor != parent:
+                    temp.append(neighbor)
+                    return True
+        return False
