26dd4cfac507c179c59e827cc7db50467d3ba7a6
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-08-06 22:57:48 +0430

    add strongly connected components

--- algorithms-on-graph/week02/graphs.py
+++ algorithms-on-graph/week02/graphs.py
@@ -1,12 +1,16 @@
 from collections import defaultdict, deque
 from collections.abc import Sequence
+from typing import TypeVar
+
+V = TypeVar("V")
 
 
 class AdjacencyList:
-    def __init__(self, n_vertices: int) -> None:
+    def __init__(self, vertices: list[V]) -> None:
+        self.vertices = vertices
         self.adj = defaultdict(list)
-        for i in range(1, n_vertices + 1):
-            self.adj[i]
+        for v in vertices:
+            self.adj[v]
 
     def add(self, v1, v2) -> None:
         self.adj[v1].append(v2)
@@ -14,11 +18,18 @@ class AdjacencyList:
     def __repr__(self) -> str:
         return f"{type(self).__name__}({repr(dict(self.adj))})"
 
+    @classmethod
+    def from_edges(cls, vertices, edges):
+        g = cls(vertices)
+        for v1, v2 in edges:
+            g.add(v1, v2)
+        return g
+
 
 # topological sort
 class Graph(AdjacencyList):
     """
-    >>> g = Graph(4)
+    >>> g = Graph([1, 2, 3, 4])
     >>> g.add(1, 2)
     >>> g.add(1, 3)
     >>> g.add(3, 4)
@@ -46,30 +57,59 @@ class Graph(AdjacencyList):
 
 
 # Strongly connected components, Naive and efficient algorithm
-class Graph(AdjacencyList):
+class Graph2(AdjacencyList):
     """
-    >>> g = Graph(4)
-    >>> g.add(1, 2)
-    >>> g.add(1, 3)
-    >>> g.add(3, 4)
-    >>> g.topological_sort()
-    deque([1, 3, 4, 2])
+    >>> g = Graph2.from_edges([0, 1, 2, 3, 4], [(0, 2), (1, 0), (2, 1), (0, 3), (3, 4)])
+    >>> g.scc()
+    [[0, 1, 2], [3], [4]]
     """
 
-    def explore(self, vertex, visited, stack):
+    def fill_order(self, vertex, visited, stack):
         visited.add(vertex)
 
         for neighbor in self.adj[vertex]:
             if neighbor not in visited:
-                self.explore(neighbor, visited, stack)
+                self.fill_order(neighbor, visited, stack)
 
-        stack.appendleft(vertex)
+        stack.append(vertex)
 
-    def topological_sort(self) -> Sequence:
+    def scc(self):
+        """Return a list of all strongly connected components."""
+        # calculate vertices sorted by reverse postorder time
         visited = set()
-        stack = deque()
+        stack = []
 
         for vertex in self.adj:
             if vertex not in visited:
-                self.explore(vertex, visited, stack)
-        return stack
+                self.fill_order(vertex, visited, stack)
+
+        # get a reversed graph
+        rev_graph = self.reversed()
+
+        # calculate the scc
+        visited = set()
+        cc = []
+
+        for v in reversed(stack):
+            if v not in visited:
+                temp = []
+                cc.append(rev_graph.explore(v, visited, temp))
+        return cc
+
+    def explore(self, v, visited, temp):
+        """this a normal DFS."""
+        visited.add(v)
+        temp.append(v)
+
+        for neighbor in self.adj[v]:
+            if neighbor not in visited:
+                temp = self.explore(neighbor, visited, temp)
+        return temp
+
+    def reversed(self):
+        """Return a reversed graph of the current graph"""
+        g = type(self)(self.vertices)
+        for v, neighbors in self.adj.items():
+            for n in neighbors:
+                g.add(n, v)
+        return g
